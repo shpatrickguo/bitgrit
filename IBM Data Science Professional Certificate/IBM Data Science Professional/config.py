33c1daa60067e75f4211663ba352554881c6eb7a
Patrick Guo <guopatrick.courses@gmail.com>
2021-10-24 23:05:08 -0700

    ibm

---
+++ IBM Data Science Professional Certificate/IBM Data Science Professional/config.py
@@ -0,0 +1,10 @@
+foursquare = {
+    'Client Id': 'ADWBCRKWDI51JK1JESRHTHZH4QIV2QQF01ZM2XQXDZH2HWWX',
+    'Client Secret': 'UM0UDBLGWVEH40LGYVC4SD2CLK014QBFXUYIXDJFSUXVHTYO'
+}
+
+datasf = {
+    'App Token': 'A4BO2RpUU4Qg0rlFpucdasnQQ',
+    'username': 'guopatrick@outlook.com',
+    'password': 'datasfAP1'
+}
